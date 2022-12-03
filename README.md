a10b23b9a58d2af3073cd28700bccb1f836a6734
Michael Geurtjens <geurtjens@me.com>
2022-12-04 08:59:28 +1000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CrozzlePackage
+
+A description of this package.
---
+++ Sources/CrozzlePackage/CrozzlePackage.swift
@@ -0,0 +1,6 @@
+public struct CrozzlePackage {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
